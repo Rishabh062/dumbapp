28ea58924aaaf73d4f7c23b47dfdc1a267c6c428
Rishabh Dwivedi <57454462+Rishabh062@users.noreply.github.com>
2022-03-03 15:21:35 +0530

    Added File

---
+++ main.js
@@ -0,0 +1,24 @@
+let message = "I knew it, you are dumb 🤣";
+let message1 = document.getElementById("write");
+
+
+function yes(){
+    message1.textContent = message;
+    return message1;
+}
+
+// Disable Inspect Element
+document.onkeydown = function(e) {
+    if(event.keyCode == 123) {
+    return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+    return false;  
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+    return false;
+    }
+    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+    return false;
+    }
+    }
